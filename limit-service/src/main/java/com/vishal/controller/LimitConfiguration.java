4f4deda151a5863ac57d2b753ca59a9dfe273bd9
Vishal Bagde <vgb@narola.email>
2023-06-02 16:48:08 +0530

    init commit

---
+++ limit-service/src/main/java/com/vishal/controller/LimitConfiguration.java
@@ -0,0 +1,28 @@
+package com.vishal.controller;
+
+public class LimitConfiguration {
+	
+	private int maximum;  
+	private int minimum;  
+	//no-argument constructor  
+	protected LimitConfiguration()  
+	{  
+	}  
+	//generating getters  
+	public int getMaximum()   
+	{  
+	return maximum;  
+	}  
+	public int getMinimum()   
+	{  
+	return minimum;  
+	}  
+	//genetrating constructor using fields  
+	public LimitConfiguration(int maximum, int minimum)   
+	{  
+	super();  
+	this.maximum = maximum;  
+	this.minimum = minimum;  
+	}  
+
+}
---
+++ limit-service/src/main/java/com/vishal/controller/LimitsConfigurationController.java
@@ -0,0 +1,21 @@
+package com.vishal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vishal.limitservice.Configuration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired    
+	private Configuration configuration; 
+	
+	@GetMapping("/limits")  
+	public LimitConfiguration retriveLimitsFromConfigurations()  
+	{  
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}  
+
+}
